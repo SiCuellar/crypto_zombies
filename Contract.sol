8d836da8d67e3aebc8081a4d3f05df75779d91c4
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2021-05-04 19:58:54 -0700

    adds require

--- Contract.sol
+++ Contract.sol
@@ -30,12 +30,15 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
-        // start here
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
+
+
 
 
 
